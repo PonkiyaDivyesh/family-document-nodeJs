93900cc755bc039cf19a2e57bc1575ea5a4402c5
Divyesh Ponkiya <divyesh.ponkiya21@gmail.com>
2023-06-17 15:16:24 +0530

    17-06-23 herokuAPP

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,8 @@ const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const rendomstring = require('randomstring');
 const sendMail = require('../helper/sendMail');
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = process.env;
 
 exports.userCreate = async (req, res) => {
     const { name, mobile_no, email, password } = req.body;
@@ -17,36 +19,38 @@ exports.userCreate = async (req, res) => {
                 if (result && result.length) {
                     return res.status(409).send({ msg: 'This mail id and mobile no is already in use!' })
                 } else {
-                    bcrypt.hash(password, 10, (err, hash) => {
-                        if (err) {
-                            return res.status(400).send({ msg: err })
-                        }
-                        else {
-                            let post = {
-                                name: name, mobile_no: mobile_no, email: email, password: hash, is_verified: 0
-                            };
-                            let sql = 'INSERT INTO account_create SET ?'
-                            pool.query(sql, post, (err, result) => {
-                                if (err) {
-                                    return res.status(400).send({ msg: err })
-                                }
-
-                                let mailSubject = 'Mail Verification';
-                                const rendomToken = rendomstring.generate();
-                                let contant = '<p>Hii ' + name + ', Please <a href="http://localhost:3002/mail-verification?token=' + rendomToken + '"> verify</a> your mail</p>';
-                                sendMail(email, mailSubject, contant);
-
-                                pool.query('UPDATE account_create SET token=? where email=?', [rendomToken, email], function (err, result) {
+                    bcrypt.genSalt(10, function (err, salt) {
+                        bcrypt.hash(password, salt, (err, hash) => {
+                            if (err) {
+                                return res.status(400).send({ msg: err })
+                            }
+                            else {
+                                let post = {
+                                    name: name, mobile_no: mobile_no, email: email, password: hash, is_verified: 0
+                                };
+                                let sql = 'INSERT INTO account_create SET ?'
+                                pool.query(sql, post, (err, result) => {
                                     if (err) {
-                                        return res.status(400).send({ msg: err });
+                                        return res.status(400).send({ msg: err })
                                     }
-                                })
 
-                                return res.status(200).send({
-                                    msg: 'User has been registered with us!',
-                                });
-                            })
-                        }
+                                    let mailSubject = 'Mail Verification';
+                                    const rendomToken = rendomstring.generate();
+                                    let contant = '<p>Hii ' + name + ', Please <a href="http://localhost:3002/mail-verification?token=' + rendomToken + '"> verify</a> your mail</p>';
+                                    sendMail(email, mailSubject, contant);
+
+                                    pool.query('UPDATE account_create SET token=? where email=?', [rendomToken, email], function (err, result) {
+                                        if (err) {
+                                            return res.status(400).send({ msg: err });
+                                        }
+                                    })
+
+                                    return res.status(200).send({
+                                        msg: 'User has been registered with us!',
+                                    });
+                                })
+                            }
+                        })
                     })
                 }
             }
@@ -60,7 +64,6 @@ exports.userCreate = async (req, res) => {
 
 exports.verifyMail = (req, res) => {
     var token = req.query.token;
-    console.log('res', res);
 
     pool.query('SELECT * FROM account_create where token=? limit 1', token, function (err, result) {
         if (err) {
@@ -77,69 +80,65 @@ exports.verifyMail = (req, res) => {
     })
 }
 
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const error = validationResult(req);
+        if (!error.isEmpty()) {
+            return res.status(400).json({ errors: error.array() });
+        }
 
+        await pool.query(`SELECT * FROM account_create WHERE email = ${pool.escape(email)};`,
+            (err, result) => {
+                if (err) {
+                    return res.status(400).send({ msg: err })
+                }
 
-// exports.studentsGet = async (req, res) => {
-//     try {
+                if (!result.length) {
+                    return res.status(401).send({ msg: 'Email or password incorrect' })
+                }
 
-//         await pool.query('SELECT * FROM contact', (err, result, fields) => {
-//             if (err) throw err;
-//             res.status(201).send(result);
+                bcrypt.compare(
+                    password,
+                    result[0]['password'],
+                    (bErr, bResult) => {
+                        if (bErr) {
+                            return res.status(400).send({ msg: bErr });
+                        }
 
-//             result.forEach(result => {
-//                 console.log(result);
-//             });
-//         })
-//     } catch (e) {
-//         res.status(400).send(e);
-//     }
-// };
+                        if (bResult) {
+                            const token = jwt.sign({ id: result[0]['id'], is_admin: result[0]['is_admin'] }, JWT_SECRET, { expiresIn: '1h' });
+                            pool.query(`UPDATE account_create SET last_login = now() WHERE id = '${result[0]['id']}'`);
+                            return res.status(200).send({ msg: 'logged In', token, user: result[0] });
+                        }
 
-// exports.studentsGetSelect = async (req, res) => {
-//     try {
-//         const _id = req.params.id;
-//         await pool.query(`SELECT * FROM contact WHERE srno=${_id}`, (err, result, fields) => {
-//             if (err) throw err;
-//             if (result.length <= 0) {
-//                 res.status(400).send('list not found');
-//             } else {
-//                 res.status(201).send(result);
-//             }
+                        return res.status(401).send({ msg: 'Email or password incorrect' });
+                    }
+                )
+            })
 
-//             result.forEach(result => {
-//                 console.log(result);
-//             });
-//         })
-//     } catch (e) {
-//         res.status(500).send(e);
-//     }
-// };
+    }
+    catch (e) {
+        console.log(e)
 
-// exports.studentsUpdate = async (req, res) => {
-//     console.log('req', req);
+        res.status(400).send(e);
+    }
+}
 
-//     try {
-//         const _id = req.params.id;
-//         let email = req.body.email;
-//         await pool.query(`UPDATE contact SET email="${email}" WHERE srno=${_id}`, (err, result, fields) => {
-//             if (err) throw err;
-//             res.status(201).send(result);
-//         })
-//     } catch (e) {
-//         console.log('updateStudent', e)
+exports.userList = async (req, res) => {
+    try {
+        const authToken = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(authToken, JWT_SECRET);
 
-//         res.status(500).send(e);
-//     }
-// };
+        await pool.query('SELECT * FROM account_create where id=?', decodedToken.id, function (err, result) {
+            if (err) throw err;
+            return res.status(200).send({ success: true, data: result[0], message: 'List get successfull' });
+        })
 
-// exports.studentsDel = async (req, res) => {
-//     try {
-//         const _id = req.params.id;
-//         await pool.query(`DELETE FROM contact WHERE srno=${_id}`, (err, result, fields) => {
-//             if (err) throw err;
-//             res.status(201).send(result);
-//         })
-//     } catch (e) {
-//         res.status(500).send(e);
-//     }
-// };
+    }
+    catch (e) {
+        console.log(e)
+
+        res.status(400).send(e);
+    }
+}
--- src/routers/userRouters.js
+++ src/routers/userRouters.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const { signupValidation, loginValidation } = require('../helper/validator');
+const { isAuthorize } = require('../middleware/auth');
 
 router.post('/user', signupValidation, userController.userCreate);
-// router.get('/login', loginValidation, studentController.login);
-// router.get('/students/:id', studentController.studentsGetSelect);
+router.post('/login', loginValidation, userController.login);
+router.get('/userList', isAuthorize, userController.userList);
 // router.patch('/students/:id', studentController.studentsUpdate);
 // router.delete('/students/:id', studentController.studentsDel);
 
